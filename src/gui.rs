bc12ded3b4aefac707ba9f1d9bb949dc6e352b8a
Arthur Lee <arthur.lee945@gmail.com>
2024-08-25 20:59:13 -0500

    updating ui

--- src/gui.rs
+++ src/gui.rs
@@ -1,4 +1,4 @@
-use eframe::egui::{self, Button, Pos2, Rect, Widget};
+use eframe::egui::{self, style::Spacing, Button, Color32, Vec2, Widget};
 
 use crate::{
     error::Error,
@@ -8,23 +8,42 @@ use crate::{
 
 pub struct Gui {
     setting: Setting,
-    // source_image: Option<egui::DroppedFile>,
+    source_image: Option<egui::DroppedFile>,
 }
 
 impl eframe::App for Gui {
     fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            let gui_rect = ui.max_rect();
+            // Main Control
             ui.horizontal(|ui| {
-                ui.horizontal_centered(|ui| {
-                    let button = Button::new("drop image here");
+                // Source Image Button
+                ui.vertical(|ui| {
+                    let size = ui.max_rect().size();
+                    let image_button =
+                        Button::new("drop image here").min_size(Vec2::new(size.x * 0.35, 100.));
+                    ui.add(image_button);
                 });
+
+                // Preview and Funnel
                 ui.vertical_centered_justified(|ui| {
-                    ui.button("sbsbsdb");
-                    ui.button("sbsbsdb");
-                    ui.button("sbsbsdb");
-                })
-            })
+                    let size = ui.max_rect().size();
+                    let spacing = ui.spacing().item_spacing;
+
+                    let (funnel_button, preview_button) = (
+                        Button::new("Funnel")
+                            .min_size(Vec2::new(100., size.y * 0.5 - spacing.y * 0.5)),
+                        Button::new("Preview")
+                            .min_size(Vec2::new(100., size.y * 0.5 - spacing.y * 0.5)),
+                    );
+
+                    ui.add(funnel_button);
+                    ui.add(preview_button);
+                });
+            });
+            ui.vertical_centered_justified(|ui| {
+                ui.painter()
+                    .rect_filled(ui.max_rect(), 10., Color32::from_rgb(219, 165, 255));
+            });
         });
         self.update_setting(ctx);
     }
@@ -34,7 +53,7 @@ impl Gui {
     pub fn new(setting: Setting) -> Self {
         Self {
             setting,
-            // source_image: None,
+            source_image: None,
         }
     }
     pub fn run(self) -> Result<()> {
