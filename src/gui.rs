e144b8fb64c8876c7f1e19a2699ad2c7a1c8bdf6
Arthur Lee <arthur.lee945@gmail.com>
2024-08-28 21:40:52 -0500

    adding messenger comp

--- src/gui.rs
+++ src/gui.rs
@@ -1,26 +1,21 @@
 use std::{ffi::OsStr, path::PathBuf, time::Duration};
 
 use eframe::egui::{self, Button, Color32, Vec2};
+use messenger::{MessageSeverity, Messenger};
 
 use crate::{
     error::Error,
     result::Result,
     setting::{config::GuiConfig, Setting},
-    sync::{sync_worker::SyncWorker, worker::Worker},
 };
 
-const SUPPORTED_FILES: [&str; 3] = ["jpg", "jpeg", "png"];
+mod messenger;
 
+const SUPPORTED_FILES: [&str; 3] = ["jpg", "jpeg", "png"];
 pub struct Gui {
     setting: Setting,
     source_image: Option<PathBuf>,
     messenger: Messenger,
-}
-
-struct Messenger {
-    message: Option<String>,
-    duration: Option<Duration>,
-    worker: SyncWorker,
 }
 
 impl eframe::App for Gui {
@@ -31,18 +26,33 @@ impl eframe::App for Gui {
                 // Source Image Button
                 ui.vertical(|ui| {
                     let button_size = Vec2::new(ui.max_rect().size().x * 0.35, 100.);
-                    let button_image = egui::Image::new(egui::include_image!("temp/profile.svg"))
+                    let button_img = egui::Image::new(egui::include_image!("temp/profile.svg"))
                         .fit_to_exact_size(button_size);
-                    let image_button = Button::image(button_image).min_size(button_size);
+                    let image_button = Button::image(button_img).min_size(button_size);
 
                     if ui.add(image_button).clicked() {
                         let Some(path) = rfd::FileDialog::new().pick_file() else {
+                            self.messenger.send_message(
+                                "No files selected".into(),
+                                Some(MessageSeverity::Warning),
+                            );
                             return;
                         };
                         let Some(extension) = path.extension().and_then(OsStr::to_str) else {
+                            self.messenger.send_message(
+                                "failed getting extension from selected file".into(),
+                                Some(MessageSeverity::Error),
+                            );
                             return;
                         };
                         if !SUPPORTED_FILES.contains(&extension) {
+                            self.messenger.send_message(
+                                format!(
+                                    "Selected files not supported format.\nSupported: [{}]",
+                                    SUPPORTED_FILES.join(", ")
+                                ),
+                                Some(MessageSeverity::Error),
+                            );
                             return;
                         }
                         self.source_image = Some(path);
@@ -64,8 +74,6 @@ impl eframe::App for Gui {
                                 .min_size(Vec2::new(100., size.y * 0.5 - spacing.y * 0.5)),
                         ),
                     );
-
-                    if mediate_button.clicked() {}
                 });
             });
 
@@ -75,7 +83,9 @@ impl eframe::App for Gui {
                     .rect_filled(ui.max_rect(), 10., Color32::from_rgb(219, 165, 255));
             });
         });
-        self.register_messenger(ctx);
+
+        let _ = self.messenger.register_messenger(ctx);
+
         self.update_setting(ctx);
     }
 }
@@ -85,11 +95,7 @@ impl Gui {
         Self {
             setting,
             source_image: None,
-            messenger: Messenger {
-                message: None,
-                duration: None,
-                worker: SyncWorker::new(Some("gui_msg_worker".into())),
-            },
+            messenger: Messenger::new(Duration::from_millis(2000)),
         }
     }
     pub fn run(self) -> Result<()> {
@@ -126,16 +132,5 @@ impl Gui {
                 self.setting.update_config_file();
             }
         })
-    }
-
-    fn register_messenger(&mut self, ctx: &egui::Context) {
-        let ctx = ctx.clone();
-        let _ = self.messenger.worker.send(move || {
-            let pos = egui::pos2(16.0, 128.0);
-            egui::Window::new("test")
-                .default_pos(pos)
-                .show(&ctx, |ui| ui.label("hihihihihihi"));
-        });
-        let _ = self.messenger.worker.recv();
     }
 }
---
+++ src/gui/messenger.rs
@@ -0,0 +1,91 @@
+use std::{
+    sync::{Arc, Mutex},
+    time::{Duration, Instant},
+};
+
+use eframe::egui::{self, Align2, Color32, Stroke, Style, Vec2};
+
+use crate::{result::Result, sync::sync_worker::SyncWorker};
+
+pub enum MessageSeverity {
+    Info,
+    Warning,
+    Error,
+}
+
+struct Message {
+    content: String,
+    severity: MessageSeverity,
+    requested_at: Instant,
+}
+
+pub struct Messenger {
+    message: Arc<Mutex<Option<Message>>>,
+    duration: Arc<Duration>,
+    worker: SyncWorker,
+}
+
+impl Messenger {
+    pub fn new(duration: Duration) -> Self {
+        Self {
+            message: Arc::new(Mutex::new(None)),
+            duration: Arc::new(duration),
+            worker: SyncWorker::new(Some("gui_messgenger_worker".into())),
+        }
+    }
+
+    pub fn register_messenger(&mut self, ctx: &egui::Context) -> Result<()> {
+        let msg_opt = self.message.clone();
+        let ctx = ctx.clone();
+        let duration = self.duration.clone();
+        self.worker.send(move || {
+            let mut msg_opt = msg_opt.lock().unwrap();
+            let Some(msg) = msg_opt.as_ref() else { return };
+            let mut open = true;
+            if Instant::now().duration_since(msg.requested_at) > *duration {
+                open = false;
+            }
+            let severity_color = match msg.severity {
+                MessageSeverity::Info => Color32::from_rgb(22, 163, 74),
+                MessageSeverity::Warning => Color32::from_rgb(202, 138, 4),
+                MessageSeverity::Error => Color32::from_rgb(200, 38, 38),
+            };
+            let msg_window = egui::Window::new("messege")
+                .open(&mut open)
+                .movable(false)
+                .resizable(false)
+                .title_bar(false)
+                .fade_in(true)
+                .fade_out(true)
+                .anchor(Align2::CENTER_TOP, Vec2::new(0., 5.))
+                .max_width(ctx.used_size().x * 0.75)
+                .frame(
+                    egui::Frame::window(&Style::default())
+                        .multiply_with_opacity(0.8)
+                        .stroke(Stroke::new(1., severity_color)),
+                )
+                .show(&ctx, |ui| {
+                    ui.label(
+                        egui::RichText::new(msg.content.clone())
+                            .monospace()
+                            .color(severity_color),
+                    )
+                });
+
+            if msg_window.is_none() {
+                *msg_opt = None;
+            }
+        })?;
+
+        self.worker.recv()?;
+        Ok(())
+    }
+
+    pub fn send_message(&mut self, msg: String, severity: Option<MessageSeverity>) {
+        *self.message.lock().unwrap() = Some(Message {
+            content: msg,
+            severity: severity.unwrap_or(MessageSeverity::Info),
+            requested_at: Instant::now(),
+        });
+    }
+}
