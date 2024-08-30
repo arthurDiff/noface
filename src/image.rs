1c5ba18326febf3c58bf593d2319a0bc690b59cc
Arthur Lee <arthur.lee945@gmail.com>
2024-08-29 21:59:51 -0500

    update cargo toml

--- src/image.rs
+++ src/image.rs
@@ -1,4 +1,6 @@
 // https://www.reddit.com/r/workingsolution/comments/xrvppd/rust_egui_how_to_upload_an_image_in_egui_and/
 // Cursor wrapped image buffer with resize fucntionality
 
-pub struct Image(image::DynamicImage);
+use image::DynamicImage;
+
+pub struct Image(DynamicImage);
